24557a6e9bca880ee020436772cc3b6f25b89dc4
ItsAlex <me@itsalex.fr>
2018-04-07 17:18:32 +0200

    Add theme

--- archlinux/scripts/install-base.sh
+++ archlinux/scripts/install-base.sh
@@ -1,38 +1,41 @@
 #!/bin/sh
 
-echo "Configure os..."
+echo "====> Configure os..."
 
-echo "Locale..."
+echo "==> Locale..."
 sudo cp ../locale/locale.conf /etc/locale.conf
 sudo locale-gen
 localctl set-locale LANG=en_US.UTF8
-echo "Sound (pulseaudio)..."
+echo "==> Sound (pulseaudio)..."
 yaourt -S pulseaudio pulseaudio-alsa pulseaudio-alsa 
 
-echo "Install features..."
+echo "===> Install features..."
 
-echo "Vim..."
+echo "==> Vim..."
 sudo pacman -S vim --noconfirm --needed
-echo "Xorg..."
+echo "==> Xorg..."
 sudo pacman -S xorg-server xorg-apps xorg-xinit --noconfirm --needed
-echo "I3 gaps..."
+echo "==> I3 gaps..."
 yaourt -S i3-gaps compton polybar-git rofi feh --noconfirm --needed
-echo "Fonts..."
+echo "==> Fonts..."
 yaourt -S monaco-powerline-font-git otf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-extra ttf-material-design-icons ttf-unifont --noconfirm --needed
-echo "Chrome..."
+echo "==> Chrome..."
 yaourt -S ttf-croscore google-chrome --noconfirm --needed
-echo "Visual studio code..."
+echo "==> Visual studio code..."
 yaourt -S visual-studio-code --noconfirm --needed
 code --install-extension Shan.code-settings-sync
-echo "Terminator..."
+echo "==> Terminator..."
 yaourt -S terminator --noconfirm --needed
-echo "Keyrings..."
+echo "==> Keyrings..."
 yaourt -S gnome-keyring --noconfirm --needed
-echo "Docker..."
+echo "==> Docker..."
 yaourt -S docker docker-compose --noconfirm --needed
 sudo usermod -aG docker $USER
 systemctl enable docker
-echo "Virtualbox..."
+echo "==> Virtualbox..."
 yaourt -S virtualbox virtualbox-host-dkms linux-headers --noconfirm --needed
-echo "Vagrant..."
+echo "==> Vagrant..."
 yaourt -S vagrant --noconfirm --needed
+
+echo "==> Install theme..."
+yaourt -S lxappearance adapta-gtk-theme
