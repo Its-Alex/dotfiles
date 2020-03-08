2dc9bc25aab558deeb519b5001466bd3da502b59
ItsAlex <me@itsalex.fr>
2020-03-08 20:13:25 +0100

    feat: update dotfile

--- archlinux/scripts/install-base.sh
+++ archlinux/scripts/install-base.sh
@@ -7,43 +7,46 @@ sudo cp ../locale/locale.gen /etc/locale.gen
 sudo locale-gen
 localectl set-locale LANG=en_US.UTF8
 echo "==> Sound (pulseaudio)..."
-yaourt -S pulseaudio pulseaudio-alsa --noconfirm --needed
+yay -S pulseaudio pulseaudio-alsa --noconfirm --needed
 
 echo "===> Install features..."
 
 echo "==> Vim..."
 sudo pacman -S vim --noconfirm --needed
 echo "==> I3 gaps..."
-yaourt -R i3blocks i3status i3-wm
-yaourt -S i3-gaps compton polybar-git rofi feh unclutter --noconfirm --needed
+yay -R i3blocks i3status i3-wm
+yay -S i3-gaps compton polybar-git rofi feh unclutter --noconfirm --needed
 echo "==> Fonts..."
-yaourt -S monaco-powerline-font-git otf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-extra ttf-material-design-icons ttf-unifont ttf-droid --noconfirm --needed
+yay -S monaco-powerline-font-git otf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-extra ttf-material-design-icons ttf-unifont ttf-droid --noconfirm --needed
 echo "==> Chrome..."
-yaourt -S ttf-croscore google-chrome --noconfirm --needed
+yay -S ttf-croscore google-chrome --noconfirm --needed
 echo "==> Visual studio code..."
-yaourt -S visual-studio-code-insiders --noconfirm --needed
-code-insiders --user-data-dir $HOME --install-extension Shan.code-settings-sync
+yay -S visual-studio-code-bin --noconfirm --needed
+code --user-data-dir "${HOME}" --install-extension Shan.code-settings-sync
 echo "==> Terminator..."
-yaourt -S terminator --noconfirm --needed
-echo "==> Keyrings..."
-yaourt -S gnome-keyring --noconfirm --needed
+yay -S terminator --noconfirm --needed
+# echo "==> Keyrings..."
+# yay -S gnome-keyring --noconfirm --needed
 echo "==> Docker..."
-yaourt -S docker docker-compose --noconfirm --needed
-sudo usermod -aG docker $USER
+yay -S docker docker-compose --noconfirm --needed
+sudo usermod -aG docker "${USER}"
 systemctl enable docker
 echo "==> Virtualbox..."
-yaourt -S virtualbox virtualbox-host-dkms linux-headers --noconfirm --needed
+yay -S virtualbox virtualbox-host-dkms linux-headers --noconfirm --needed
 echo "==> Vagrant..."
-yaourt -S vagrant --noconfirm --needed
+yay -S vagrant --noconfirm --needed
 echo "==> Spotify..."
-yaourt -S spotify --noconfirm --needed
+yay -S spotify --noconfirm --needed
 
 echo "==> Grub update..."
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 echo "==> Theme..."
-yaourt -S lxappearance adapta-gtk-theme --noconfirm --needed
+yay -S lxappearance adapta-gtk-theme --noconfirm --needed
 echo "==> Construct home..."
 mkdir -p ~/Documents
 mkdir -p ~/Downloads
 cp -R ../Pictures ~/
+
+yay -S zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
--- setup.sh
+++ setup.sh
@@ -3,22 +3,21 @@
 use () {
     if [ $1 -lt 2 ]
         then
-        echo -e "Usage:\n  "$0" [mode] [config]\n"
+        echo -e "Usage:\n  $0 [mode] [config]\n"
         echo -e "Params:\n  config\tname of the configuration to backup\n  mode\t\tsync to synchronize - backup to backup\n"
-        echo -e "Example:\n  "$0" sync desktop"
+        echo -e "Example:\n  $0 sync desktop"
         exit 0
     fi
 }
 
 sync () {
-    cp -R $2/.config/* $HOME/.config
-    cp -R $2/.local/share/rofi/* $HOME/.local/share/rofi
-    cp -R $2/.vim $HOME
-    cp $2/.zshrc $HOME/.zshrc
-    cp $2/.vimrc $HOME/.vimrc
-    cp $2/.gitconfig $HOME/.gitconfig
+    cp -R $2/.config/* "$HOME/.config"
+    cp -R $2/.local/share/rofi/* "$HOME/.local/share/rofi"
+    cp -R "$2/.vim" "$HOME"
+    cp "$2/.vimrc" "$HOME/.vimrc"
+    cp "$2/.gitconfig" "$HOME/.gitconfig"
 
-    sudo cp $2/etc/pam.d/login /etc/pam.d/login
+    curl https://gist.githubusercontent.com/Its-Alex/cf80dca9855e9a7fbf64b9fc67c2ad05/raw/ > ~/.zshrc
 }
 
 backup () {
@@ -28,17 +27,14 @@ backup () {
     mkdir -p /etc/X11/xinit
     mkdir -p /etc/X11/xinit/xinitrc.d
 
-    cp -R $HOME/.config/i3 $2/.config/
-    cp -R $HOME/.config/polybar $2/.config/
-    cp -R $HOME/.config/compton $2/.config/
-    cp -R $HOME/.config/terminator $2/.config/
-    cp -R $HOME/.local/share/rofi/themes $2/.local/share/rofi/
-    cp -R $HOME/.vim/colors $2/.vim/
-    cp $HOME/.zshrc $2/.zshrc
-    cp $HOME/.vimrc $2/.vimrc
-    cp $HOME/.gitconfig $2/.gitconfig
-
-    sudo cp /etc/pam.d/login $2/etc/pam.d/login
+    cp -R "$HOME/.config/i3" "$2/.config/"
+    cp -R "$HOME/.config/polybar" "$2/.config/"
+    cp -R "$HOME/.config/compton" "$2/.config/"
+    cp -R "$HOME/.config/terminator" "$2/.config/"
+    cp -R "$HOME/.local/share/rofi/themes" "$2/.local/share/rofi/"
+    cp -R "$HOME/.vim/colors" "$2/.vim/"
+    cp "$HOME/.vimrc" "$2/.vimrc"
+    cp "$HOME/.gitconfig" "$2/.gitconfig"
 }
 
 use $#
@@ -46,11 +42,11 @@ use $#
 case $1 in
     "sync")
         echo "Sync in progress..."
-        sync $1 $2
+        sync "$1" "$2"
         ;;
     "backup")
         echo "Backup in progress..."
-        backup $1 $2
+        backup "$1" "$2"
         ;;
     *)
         echo "Unknown mode"
