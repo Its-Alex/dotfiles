3f3dbe5f24ac49296e2b52ec0b883ac96e5620f3
ItsAlex <me@itsalex.fr>
2024-01-17 10:49:35 +0100

    chore: update achlinux-gnome-install script

--- archlinux-gnome-install.sh
+++ archlinux-gnome-install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "====> Configure os..."
+printf "\e[1;34m%s\e[1;0m\n" "Configure os..."
 
-echo "==> Install Git"
+printf "\e[1;34m%s\e[1;0m\n" "Install Git..."
 sudo pacman -S git base-devel
 cp ../files/.gitconfig "${HOME}/.gitconfig"
-echo "==> Install Yay"
+printf "\e[1;34m%s\e[1;0m\n" "Install Yay..."
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 (
     cd /tmp/yay || exit 1
@@ -13,60 +13,55 @@ git clone https://aur.archlinux.org/yay.git /tmp/yay
 )
 rm -rf /tmp/yay
 
-echo "==> Keyboard..."
-yay -S xkb-qwerty-fr --noconfirm --needed
+printf "\e[1;34m%s\e[1;0m\n" "Install needed programs.."
+yay -S --noconfirm --needed xkb-qwerty-fr \
+    vim \
+    otf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-extra ttf-material-design-icons ttf-unifont ttf-droid ttf-fira-code \
+    alacritty tmux tmuxinator \
+    gnome-keyring \
+    docker \
+    virtualbox virtualbox-host-dkms linux-headers \
+    vagrant \
+    zsh \
+    flatpak
 
-echo "===> Install needed programs..."
-
-echo "==> Vim..."
-sudo pacman -S vim --noconfirm --needed
-echo "==> Fonts..."
-yay -S monaco-powerline-font-git otf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-extra ttf-material-design-icons ttf-unifont ttf-droid ttf-fira-code --noconfirm --needed
-echo "==> Visual studio code..."
-# yay -S visual-studio-code-bin --noconfirm --needed
-code --user-data-dir "${HOME}" --install-extension Shan.code-settings-sync
-echo "==> alacritty..."
-yay -S alacritty tmux tmuxinator --noconfirm --needed
-mkdir -p "${HOME}/.tmux/plugins"
-git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
-echo "==> Keyrings..."
-yay -S gnome-keyring --noconfirm --needed
-echo "==> Docker..."
-yay -S docker --noconfirm --needed
-sudo usermod -aG docker "${USER}"
-systemctl enable docker
-echo "==> Virtualbox..."
-yay -S virtualbox virtualbox-host-dkms linux-headers --noconfirm --needed
-echo "==> Vagrant..."
-yay -S vagrant --noconfirm --needed
-
-echo "==> Grub update..."
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-
-yay -S zsh --noconfirm --needed
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "===> Install customs programs..."
-
+printf "\e[1;34m%s\e[1;0m\n" "Install custom programs.."
 yay -S --noconfirm --needed \
-    numix-cursor-theme \
-    numix-frost-themes \
-    numix-square-icon-theme \
+    numix-cursor-theme numix-frost-themes numix-square-icon-theme flat-remix-gnome \
     unzip \
     tar \
     fzf \
-    vlc \
-    authy \
-    spotify \
-    slack-desktop \
-    joplin-desktop \
-    element-desktop
+    vlc
 
+flatpak -y install flathub org.mozilla.firefox \
+    com.google.Chrome \
+    org.signal.Signal \
+    com.slack.Slack \
+    com.discordapp.Discord \
+    com.spotify.Client \
+    net.cozic.joplin_desktop \
+    im.riot.Riot \
+    com.visualstudio.code \
+    com.authy.Authy \
+    org.videolan.VLC
+
+printf "\e[1;34m%s\e[1;0m\n" "Setup programs.."
+# Setup tmux
+mkdir -p "${HOME}/.tmux/plugins"
+git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
+# Setup docker
+sudo usermod -aG docker "${USER}"
+sudo systemctl enable docker
+# Setup grub for Windows boot
+echo "==> Grub update for Microsoft boot..."
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+# Setup ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Install https://github.com/jdx/mise
 curl https://mise.jdx.dev/mise-latest-linux-x64 > /tmp/mise
 sudo mv /tmp/mise /usr/local/bin
 
 # Nvidia
-
 # yay -S --noconfirm --needed \
 #     nvidia \
 #     nvidia-dkms \
