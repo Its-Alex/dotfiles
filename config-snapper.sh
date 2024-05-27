52a1fafd5683236bb0875b1f33fefb1749954f25
ItsAlex <me@itsalex.fr>
2024-05-27 15:13:17 +0200

    fix: add missing inotify-tools to make grub-btrfs works

--- config-snapper.sh
+++ config-snapper.sh
@@ -6,7 +6,7 @@ if [[ "$EUID" -ne 0 ]]; then
     exit 1
 fi
 
-yay -S --noconfirm --needed snapper grub-btrfs
+yay -S --noconfirm --needed snapper grub-btrfs inotify-tools
 
 if [[ "$(snapper list-configs | grep -c root)" == "0" ]]; then
     # Umount default subvolume /.snapshots to let snapper create subvolume
