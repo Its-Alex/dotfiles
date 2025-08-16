11f0e5a4773aa005ce452d882c12acf86ad3ba42
ItsAlex <me@itsalex.fr>
2025-08-16 14:34:36 +0200

    chore: update gnome extensions

--- scripts/backup-gnome-extensions.sh
+++ scripts/backup-gnome-extensions.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 cd "$(dirname "$0")/../"
 
 # Save extensions
-gnome-extensions list --enabled > "./extensions-enabled.txt"
+gnome-extensions list --enabled > "./gnome-extensions.txt"
 
 # Save extensions settings
 dconf dump /org/gnome/shell/extensions/ > "./gnome-shell-extensions.dconf"
