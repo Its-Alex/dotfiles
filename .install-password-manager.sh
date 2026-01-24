0b09db0c45b136d41d2f79b7bd1322a99673d56e
ItsAlex <me@itsalex.fr>
2026-01-24 23:46:38 +0100

    feat: make install-password-manager more robust across distribution

--- .install-password-manager.sh
+++ .install-password-manager.sh
@@ -5,7 +5,14 @@ type bw >/dev/null 2>&1 && exit
 
 case "$(uname -s)" in
 Linux)
-    if [ -f /etc/arch-release ]; then
+    # Check os-release in standard locations
+    _os_id=""
+    if [ -f /etc/os-release ]; then
+        _os_id="$(. /etc/os-release && echo "$ID")"
+    elif [ -f /usr/lib/os-release ]; then
+        _os_id="$(. /usr/lib/os-release && echo "$ID")"
+    fi
+    if [ "$_os_id" = "arch" ]; then
         sudo pacman -S --noconfirm --needed bitwarden-cli
         if [ "$CHEZMOI_PASSWORD_MANAGER_DISABLED" != "true" ] && [ "$CHEZMOI_PASSWORD_MANAGER_DISABLED" != "1" ]; then
             bw login
