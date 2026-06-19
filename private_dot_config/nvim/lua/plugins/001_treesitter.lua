ff1ca406e177ceecee83f09cd98b862e33d91343
ItsAlex <me@itsalex.fr>
2026-06-19 20:55:59 +0200

    chore: update nvim configuration

--- private_dot_config/nvim/lua/plugins/001_treesitter.lua
+++ private_dot_config/nvim/lua/plugins/001_treesitter.lua
@@ -1,57 +1,18 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
+    -- nvim-treesitter is used ONLY as a parser installer.
+    -- Highlighting, indent, folds and incremental selection
+    -- are handled natively by Neovim 0.12 (see plugin/treesitter.lua).
     config = function()
         require('nvim-treesitter.configs').setup({
             ensure_installed = {
-                -- Lua
-                "lua",
-                -- Vim
-                "vim",
-                "vimdoc",
-                -- Bash
-                "bash",
-                -- Helm
-                'helm',
-                -- HCL
-                'hcl',
-                -- Python
-                "python",
-                -- CSS
-                "css",
-                -- HTML
-                "html",
-                -- Javascript
-                "typescript",
-                "javascript",
-                -- Graphql
-                "graphql",
-                -- Svelte
-                "svelte",
-                -- Golang
-                "go",
-                -- Markdown
-                "markdown",
-                -- Ninja
-                "ninja",
-                -- SQL
-                "sql",
-                -- Data exchange format
-                "json",
-                "toml",
-                "yaml",
-            },
-            auto_install = false,
-            sync_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = true,
-            },
-            indent = {
-                enable = true
-            },
-            incremental_selection = {
-                enable = true
+                "lua", "vim", "vimdoc", "bash",
+                "helm", "hcl", "python", "css", "html",
+                "typescript", "javascript", "graphql", "svelte",
+                "go", "markdown", "markdown_inline", "ninja", "sql",
+                "json", "toml", "yaml",
             },
         })
     end
--- private_dot_config/nvim/lua/plugins/002_lsp.lua
+++ private_dot_config/nvim/lua/plugins/002_lsp.lua
@@ -15,43 +15,43 @@ return {
             require('mason-tool-installer').setup({
                 ensure_installed = {
                     -- Lua
-                    { 'lua_ls', version = '3.13.5'},
+                    { 'lua_ls', version = '3.18.2'},
                     -- Vim
                     { 'vim-language-server', version = '2.3.1'},
                     -- Yaml
-                    { 'yamlls', version = '1.15.0'},
+                    { 'yamlls', version = '1.23.0'},
                     -- Json
                     { 'json-lsp', version = '4.10.0'},
                     -- SQL
                     { 'sqlls', version = '1.7.1'},
-                    { 'sql-formatter', version = '15.4.9'},
+                    { 'sql-formatter', version = '15.8.1'},
                     -- Bash
-                    { 'shellcheck', version = 'v0.10.0'},
-                    { 'bash-language-server', version = '5.3.4'},
+                    { 'shellcheck', version = 'v0.11.0'},
+                    { 'bash-language-server', version = '5.6.0'},
                     -- Helm
-                    { 'helm-ls', version = 'v0.1.1'},
+                    { 'helm-ls', version = 'v0.5.4'},
                     -- Markdown
-                    { 'markdownlint-cli2', version = '0.17.1'},
+                    { 'markdownlint-cli2', version = '0.22.1'},
                     -- Ansible
-                    { 'ansible-language-server', version = '1.2.3'},
+                    { 'ansible-language-server', version = '26.6.0'},
                     -- Terraform
-                    { 'terraform-ls', version = 'v0.36.3'},
+                    { 'terraform-ls', version = 'v0.38.7'},
                     -- CSS
                     { 'css-lsp', version = '4.10.0'},
                     -- HTLM
                     { 'html-lsp', version = '4.10.0'},
                     -- Python
-                    { 'pyright', version = '1.1.392'},
+                    { 'pyright', version = '1.1.410'},
                     { 'autopep8', version = '2.3.2'},
-                    { 'flake8', version = '7.1.1'},
-                    { 'isort', version = '5.13.2'},
+                    { 'flake8', version = '7.3.0'},
+                    { 'isort', version = '8.0.1'},
                     -- Javascript
-                    { 'prettierd', version = '0.26.0'},
-                    { 'eslint_d', version = '14.3.0'},
+                    { 'prettierd', version = '0.28.0'},
+                    { 'eslint_d', version = '15.0.2'},
                     -- Svelte
-                    { 'svelte-language-server', version = '0.17.10'},
+                    { 'svelte-language-server', version = '0.18.2'},
                     -- Typescript
-                    { 'typescript-language-server', version = '4.3.3'}
+                    { 'typescript-language-server', version = '5.3.0'}
                 },
                 auto_update = false,
                 run_on_start = true
