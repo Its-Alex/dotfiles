5e453bf21469c108b6889dbf13d22255c0d9e5aa
ItsAlex <me@itsalex.fr>
2026-06-21 22:00:12 +0200

    fix: bad syntax for nvim tree-sitter

--- private_dot_config/nvim/lua/plugins/001_treesitter.lua
+++ private_dot_config/nvim/lua/plugins/001_treesitter.lua
@@ -6,14 +6,12 @@ return {
     -- Highlighting, indent, folds and incremental selection
     -- are handled natively by Neovim 0.12 (see plugin/treesitter.lua).
     config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                "lua", "vim", "vimdoc", "bash",
-                "helm", "hcl", "python", "css", "html",
-                "typescript", "javascript", "graphql", "svelte",
-                "go", "markdown", "markdown_inline", "ninja", "sql",
-                "json", "toml", "yaml",
-            },
+        require('nvim-treesitter').install({
+            "lua", "vim", "vimdoc", "bash",
+            "helm", "hcl", "python", "css", "html",
+            "typescript", "javascript", "graphql", "svelte",
+            "go", "markdown", "markdown_inline", "ninja", "sql",
+            "json", "toml", "yaml",
         })
     end
 }
