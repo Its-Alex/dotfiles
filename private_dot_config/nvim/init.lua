9ba58d4820980265cbb6b54b82069a3e3a77122a
ItsAlex <me@itsalex.fr>
2026-07-29 17:40:12 +0200

    chore: disable lazy.nvim update checker

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -15,5 +15,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-  checker = { enabled = true, auto_install = false },
+  checker = { enabled = false, auto_install = false },
 })
